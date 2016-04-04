d2f144a0e68376b1629a014fe2fe2862851f01ff
anneg1000 <anneg1000@hotmail.com>
2016-04-03 18:27:43 -0700

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -18,17 +18,17 @@ files <- list.files(destdir, recursive=TRUE)
 files
 
 ## 1. Merges the training and the test sets to create one data set
-
-trainx <- read.table("mydata/UCI HAR Dataset/train/X_train.txt")
-testx  <- read.table("mydata/UCI HAR Dataset/test/X_test.txt" )
+library(data.table)
+trainx <- read.table("UCI HAR Dataset/train/X_train.txt")
+testx  <- read.table("UCI HAR Dataset/test/X_test.txt" )
 featuredata <- rbind(trainx, testx)
 
-trainy <- read.table("mydata/UCI HAR Dataset/train/Y_train.txt")
-testy  <- read.table("mydata/UCI HAR Dataset/test/Y_test.txt")
+trainy <- read.table("UCI HAR Dataset/train/Y_train.txt")
+testy  <- read.table("UCI HAR Dataset/test/Y_test.txt")
 activitydata <- rbind(trainy, testy)
 
-trains <- read.table("mydata/UCI HAR Dataset/train/subject_train.txt")
-tests  <- read.table("mydata/UCI HAR Dataset/test/subject_test.txt")
+trains <- read.table("UCI HAR Dataset/train/subject_train.txt")
+tests  <- read.table("UCI HAR Dataset/test/subject_test.txt")
 subjectdata <- rbind(trains, tests)
 
 str(featuredata)
@@ -36,8 +36,7 @@ str(activitydata)
 str(subjectdata)
 
 ## 2. Extracts only the measurements on the mean and standard deviation for each measurement
-
-features <- read.table("mydata/UCI HAR Dataset/features.txt")
+features <- read.table("UCI HAR Dataset/features.txt")
 cleanfeatures <- gsub("([()])", "", features[,2])
 cleanfeatures <- gsub("([-])","", cleanfeatures)
 cleanfeatures <- gsub("([,])","", cleanfeatures)
@@ -45,12 +44,10 @@ colnames(featuredata) <- cleanfeatures
 
 idx <- grep("mean\\(\\)|std\\(\\)", features$V2)
 featuredata <- featuredata[, idx]
-
 head(featuredata, 10)
 
 ## 3. Uses descriptive activity names to name the activities in the data set
-
-activitylabels <- read.table("mydata/UCI HAR Dataset/activity_labels.txt")
+activitylabels <- read.table("UCI HAR Dataset/activity_labels.txt")
 activitydata[,1] = activitylabels[activitydata[,1], 2]
 names(activitydata) <- "activity"
 
@@ -58,9 +55,7 @@ head(activitydata, 10)
 tail(activitydata, 10)
 
 ## 4. Appropriately labels the data set with descriptive variable names
-
 names(subjectdata) <- "subject"
-
 head(subjectdata,10)
 tail(subjectdata,10)
 
@@ -79,7 +74,6 @@ names(combined)
 write.table(combined, file="tidydata.txt", row.names=FALSE)
 
 ## 5. creates a second, independent tidy data set with the average of each variable for each activity and each subject
-
 library(plyr)
 names(combined) <- tolower(names(combined))
 avg <- aggregate(. ~subject + activity, combined, mean)
@@ -88,8 +82,7 @@ write.table(avg, file="tidydata_with_average.txt", row.names=FALSE)
 head(avg,20)
 
 # reshape the data using melt() to generate long-format data based on the activity and subject for all measurements
-#library(reshape2)
-#names(avg) <- tolower(names(avg))
-#avg2 <- melt(avg, id=c("subject", "activity"))
-#write.table(avg2, file="tidydata_with_average_long.txt", row.names=FALSE)
-#head(avg,20)
+library(reshape2)
+avg2 <- melt(avg, id=c("subject", "activity"))
+write.table(avg2, file="tidydata_with_average_long.txt", row.names=FALSE)
+head(avg,20)
